f63c7318f86abe89a4c40139f56692c3d1180011
Esmeralda Espinoza-Jardine <esmeralda.espinoza-jardine@woodmac.com>
2024-04-29 14:07:42 +0100

    starting point

---
+++ tutorial_4_1_model_sol/client.py
@@ -0,0 +1,20 @@
+import sys
+import socket
+
+server_addr = (sys.argv[1], int(sys.argv[2])) 
+
+server_addr_str = str(server_addr)
+
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    print("Connecting to " + server_addr_str + "... ")
+    client_socket.connect(server_addr)
+except Exception as e:
+    print(e)
+    exit(1)
+    
+    
+    
+

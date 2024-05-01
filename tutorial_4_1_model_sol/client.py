c1095e08ae25fcbb6b269f9f169660cd809a5ca8
Dannielle Villanueva <6874781+Emenopi@users.noreply.github.com>
2024-05-01 18:20:06 +0100

    cant remember

--- tutorial_4_1_model_sol/client.py
+++ tutorial_4_1_model_sol/client.py
@@ -8,33 +8,33 @@ request_type_str = str(sys.argv[3])
 server_addr_str = str(server_addr)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def verify_acknowledgement(message, message_type):
-    if message.decode()[:3] != "ack":
-        print("Server did not acknowledge the " + message_type + " message. Exiting...")
-        exit(1)
-    elif message_type != "content":
-        print(message_type + " sent")
+def handshake(socket):
+    send_one_message(socket, "syn")
+    response = recv_one_message(socket).decode()
+    if response == "syn/ack":
+        send_one_message(socket, "ack")
+        print("Handhsake successful...continuing...")
+        return
     else:
-        print(message_type + " received")
-        
-
-
+        print("Handshake failed...")
+        socket.close()
+        exit(0)
 try:
     print("Connecting to " + server_addr_str + "... ")
     client_socket.connect(server_addr)
 except Exception as e:
     print(e)
+    socket.close()
     exit(1)
     
     
 try:
     while True:
+        handshake(client_socket)
         request_type_sent = send_one_message(client_socket, request_type_str)
-        verify_acknowledgement(client_socket.recv(1024), "request-type")
 
 
         if request_type_str == "list":
-            verify_acknowledgement(client_socket.recv(1024), "files")
             files = recv_one_message(client_socket).decode()
             print("files: ", files)
             break
@@ -43,21 +43,19 @@ try:
         else:
             filename_str = str(sys.argv[4])
             filename_sent = send_one_message(client_socket, filename_str) 
-            verify_acknowledgement(client_socket.recv(1024), "filename")
-        if request_type_str == "put":  
-            file_path = get_path("client_data", True) + "/" + filename_str
-            data_sent = handle_request(request_type_str, file_path, client_socket)
-            print("file sent successfully. Exiting...")
-            break
+            if request_type_str == "put":  
+                file_path = get_path("client_data", True) + "/" + filename_str
+                data_sent = handle_request(request_type_str, file_path, client_socket)
+                print("file sent successfully. Exiting...")
+                break
 
-        elif request_type_str == "get":
-            verify_acknowledgement(client_socket.recv(1024), "content")
-            content = recv_one_message(client_socket).decode()
-            print("file name: ", filename_str)
-            file_path = get_path("client_data", True) + "/" + filename_str
-            print("file path: ", file_path)
-            new_file = write_to_file(file_path, content)
-            break
+            elif request_type_str == "get":
+                content = recv_one_message(client_socket).decode()
+                print("file name: ", filename_str)
+                file_path = get_path("client_data", True) + "/" + filename_str
+                print("file path: ", file_path)
+                new_file = write_to_file(file_path, content)
+                break
 
 
 
--- tutorial_4_1_model_sol/server.py
+++ tutorial_4_1_model_sol/server.py
@@ -1,6 +1,6 @@
 import sys
 import socket
-from common_methods import handle_request, get_path, write_to_file, recv_one_message
+from common_methods import handle_request, get_path, send_one_message, write_to_file, recv_one_message
 
 def parse_port_arg():
 
@@ -25,6 +25,22 @@ except Exception as e:
     print(e)
     exit(1)
     
+def handshake(socket):
+    response = recv_one_message(socket).decode()
+    if response == "syn":
+        send_one_message(socket, "syn/ack")
+        ack_response = recv_one_message(socket).decode()
+        if ack_response == "ack":
+            print("Handshake successful, continuing..")
+        else:
+            print("Handshake failed on ack...")
+            socket.close()
+            exit()
+        
+    else:
+        print("Handshake failed on syn...")
+        socket.close()
+        exit(0)
     
 try:
     while True:
@@ -32,41 +48,43 @@ try:
         client_socket, client_address = server_socket.accept()
         cli_addr_str = str(client_address) 
         print("Client " + cli_addr_str + " connected. Now chatting...")
-        
+        handshake(client_socket)
 
         request_type_str = recv_one_message(client_socket).decode()
-        client_socket.sendall(str.encode("ack"))
 
         if request_type_str == "list":
+            print("Sending file list...")
             file_path = get_path("server_data", False)
-            client_socket.sendall(str.encode("ack"))   
             sent_data = handle_request(request_type_str, file_path, client_socket)
             
         
         else:
             second_message = recv_one_message(client_socket).decode()
-            client_socket.sendall(b'ack')
             print("second message: ", second_message)
 
         if request_type_str == "put":
+            print("Server receiving file...")
             filename_str = second_message
             content = recv_one_message(client_socket).decode()
             print("message : ", content)
             file_path = get_path("server_data", False) + "/" + filename_str
             new_file = write_to_file(file_path, content)
             print("file saved")
+            exit(1)
             
         
         elif request_type_str == "get":
+            print("Server sending file...")
             filename_str = second_message
             request_type_str = "put"
             file_path = get_path("server_data", False) + "/" + filename_str
-            client_socket.sendall(str.encode("ack"))
             sent_data = handle_request(request_type_str, file_path, client_socket)
+            print("file sent!")
+            exit(1)
 
 except Exception as e:
     print("Error message: ", e, " occurred. Exiting...")
-    exit(1)           
+    exit(0)           
 
 
         
